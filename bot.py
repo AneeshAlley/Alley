18003d0913d619f27e4225a8ccb3ce598505663d
AneeshAlley <82529977+AneeshAlley@users.noreply.github.com>
2022-03-23 19:55:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,13 +6,16 @@ from dotenv import load_dotenv
 import os
 import random
 
-
-bot = commands.Bot(command_prefix="rok?")
+intents = discord.Intents.default()  
+intents.members = True
+bot = commands.Bot(command_prefix="rok?", intents=intents)
 
 load_dotenv()
 
 @bot.event
 async def on_ready():
+    activity=discord.Game(name="Love Recoon | rok?bh")
+    await bot.change_presence(activity=activity)
     print(f"{bot.user.name} started!")
 
 @bot.command()
@@ -42,70 +45,35 @@ async def rock(ctx):
 
 @bot.command()
 async def owner(ctx):
-   await ctx.send("Recoon is my boss")
+   own = ctx.guild.owner
+   e = discord.Embed(title=f"{own}", description="Recoon is the boss of the world!")
+   await ctx.send(embed=e)
 
 @bot.command()
 async def gaming(ctx):
-    await ctx.send("join games channel for gaming")
+    e = discord.Embed(title="Gaming channel", description="Come to my Gaming channel<#894928863992361031>")
+    await ctx.send(embed=e)
 
+@bot.command(name="ping", pass_context=True, aliases=["latency", "latence"])
+async def ping(ctx):
+    
+    embed = discord.Embed(title="__**Latence**__", colour=discord.Color.dark_gold(), timestamp=ctx.message.created_at)
+    embed.add_field(name="Latence of bot :", value=f"`{round(bot.latency * 1000)} ms`")
 
+    await ctx.send(embed=embed)
 
-@bot.command()
-async def balance(ctx):
-  await open_account(ctx.author)
-
-  user = ctx.author
-
-  users = await get_bank_data()
-  
-  wallet_amt= users[str(user.id)]["Wallet"]
-  bank_amt= users[str(user.id)]["Bank"]
-
-
-  em = discord.Embed(title=f"{ctx.author.name}'s balance.", color=discord.Color.teal()) 
-  em.add_field(
-    name="Wallet Balance",value=wallet_amt
-  )
-  em.add_field(
-    name="Bank Balance",value=bank_amt
-  )
-  await ctx.send(embed=em)
-
-async def open_account(user):
-  users = await get_bank_data()
-  
-  if str(user.id) in users:
-    return False
-  else:
-    users[str(user.id)] = {}
-    users[str(user.id)]["Wallet"] = 0
-    users[str(user.id)]["Bank"] = 0
-  
-  with open("bank.json",'w') as f:
-    users = json.dump(users,f)
-  return True
-
-async def get_bank_data():
-  with open("bank.json",'r') as f:
-    users = json.load(f)
-  return users
-
-@bot.command()
-async def beg(ctx):
-  await open_account(ctx.author)
-
-  user = ctx.author
-
-
-  users = await get_bank_data()
-
-  earnings = random.randrange(101)
-  await ctx.send(f"Someone gave your {earnings} coins")
-
-  users[str(user.id)]["Wallet"] += earnings
-
-  with open("bank.json",'r') as f:
-    users = json.dump(users,f)
+@bot.command(aliases=["bh"])
+async def bothelp(ctx):
+    e = discord.Embed(title="Help! all commands", description="My all commands with how to use.")
+    e.set_author(name=f"{bot.user.name}", icon_url=f"{bot.user.avatar_url}")
+    e.add_field(name="`rok?bothelp`", value="Shows this message.")
+    e.add_field(name="`rok?gaming`", value="Takes you to gaming channel.")
+    e.add_field(name="`rok?meme`", value="Sends random memes.")
+    e.add_field(name="`rok?owner`", value="Owner is invincible.")
+    e.add_field(name="`rok?ping`", value="Shows my ping.")
+    e.add_field(name="`rok?rock`", value="Sends random rock images.")
+    e.add_field(name="`rok?test`", value="Normal test command.")
+    await ctx.send(embed=e)        
 
  
 
